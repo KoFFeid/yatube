b38b62757f4e6c5b445622c43338d168d2854aeb
Akimov Dmitry <akimov92@yandex.ru>
2022-06-24 16:38:14 +0300

    add admin and models

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,8 +6,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'posts/index.html')
+    context = {'title':"Это главная страница проекта Yatube"}
+    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, slug):
-    return render(request, 'posts/group_list.html')
+def group_posts(request, pk):
+    context = {'title':"Здесь будет информация о группах проекта Yatube"}
+    return render(request, 'posts/group_list.html', context)
