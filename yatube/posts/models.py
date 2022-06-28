6875e5946a92b2a564d5099d10a922bbbef0c90f
Akimov Dmitry <akimov92@yandex.ru>
2022-06-28 16:51:43 +0300

    add Group

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,12 +5,26 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length = 250)
+    slug = models.SlugField(max_length = 50)
+    description = models.TextField()
+
+
+    def __str__(cls):
+        return cls.title
+        
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    models.E
+
     author = models.ForeignKey(
         User,
         on_delete = models.CASCADE,
         related_name= 'posts' 
     )
+
+    group = models.ForeignKey( Group, blank=True, null=True,
+     on_delete=models.CASCADE)
+
+
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 
 
 # Create your views here.
 
-def index(request):
-    context = {'title':"Это главная страница проекта Yatube"}
-    return render(request, 'posts/index.html', context)
+#def index(request):
+#    context = {'title':"Это главная страница проекта Yatube"}
+#    return render(request, 'posts/index.html', context)
 
 
-def group_posts(request, pk):
-    context = {'title':"Здесь будет информация о группах проекта Yatube"}
+def group_posts(request, slug):
+    
+    posts = Post.objects.filter('').order_by('-pub_date')[:10]
+    context = {'title':
+        f"Посты группы: {slug}",
+        'posts' : posts}
     return render(request, 'posts/group_list.html', context)
+
+def index(request):
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
